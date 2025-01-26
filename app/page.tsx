82e0ca2f95bb00de712f6cff40b4357e8fcd7ab8
Kwabena Andoh-Baidoo <knandohb@gmail.com>
2025-01-26 00:06:45 -0600

    init

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default async function Home() {
 	return (
 		<main>
-			<div>Home Page</div>
+			<div>Home Page New</div>
 			<div>
 				<Link href='/'>Home</Link>
 			</div>
