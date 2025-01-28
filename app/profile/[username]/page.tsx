3f4070463ebe0ad7e519396a8e99d570eae9f3f5
Kwabena Andoh-Baidoo <knandohb@gmail.com>
2025-01-28 16:22:08 -0600

    ANother attempt at auth deployment

--- app/profile/[username]/page.tsx
+++ app/profile/[username]/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default async function ProfilePage({
 	params,
 }: {
-	params: { username: string };
+	params: Promise<{ username: string }>;
 }) {
 	const supabase = await createClient();
 	const { username } = await params;
